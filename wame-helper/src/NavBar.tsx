22d912cb8245c6687c7cde2b39c697f64c6395a5
Lawson Kwek <lawson.kwek@fdmgroup.com>
2026-01-13 12:24:08 +0800

    Removed placeholder image

--- wame-helper/src/NavBar.tsx
+++ wame-helper/src/NavBar.tsx
@@ -3,13 +3,13 @@ const NavBar = () => {
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <img
+          {/* <img
             src="/docs/5.3/assets/brand/bootstrap-logo.svg"
             alt="Logo"
             width="30"
             height="24"
             className="d-inline-block align-text-top"
-          ></img>
+          ></img> */}
           wame-helper
         </a>
       </div>
